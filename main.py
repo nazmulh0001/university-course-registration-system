36dc0d646713d946bc8aff4f59a136324b11ad23
nazmulh0001 <nazmulhasangub001@gmail.com>
2026-04-29 18:06:47 +0600

    course class

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+import csv
+
+#Course Class
+class Course:
+    def __init__(self, code, title, credits, prerequisite):
+        self.code = code
+        self.title = title
+        self.credits = float(credits)
+        self.prerequisite = prerequisite
+
+    def __str__(self):
+        return f"{self.code} - {self.title} ({self.credits} credits) | Prereq: {self.prerequisite}"
